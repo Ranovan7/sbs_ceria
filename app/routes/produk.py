02a8facd2ed7191ad99c9a002965bdc80e5ade5c
Ranovan7 <romyranovan@gmail.com>
2021-02-18 10:11:34 +0700

    remove unused css and js, added produk search for obat nama and batch

--- app/routes/produk.py
+++ app/routes/produk.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request, Depends, Form, Cookie, status
 from fastapi.responses import RedirectResponse, HTMLResponse
+from sqlalchemy import or_
 from sqlalchemy.orm import Session
 from app import templates
 from app.models import Barang, Obat
@@ -16,9 +17,30 @@ router = APIRouter(
 
 
 @router.get("/", dependencies=[Depends(admin_only)])
-async def index(request: Request, user = Depends(current_user), db: Session = Depends(db_session), message = Depends(get_message)):
+async def index(request: Request, query: str = None, user = Depends(current_user), db: Session = Depends(db_session), message = Depends(get_message)):
     obats = db.query(Obat).order_by(Obat.id).all()
-    barangs = db.query(Barang).order_by(Barang.id).all()
+
+    if not query:
+        barangs = db.query(Barang).order_by(Barang.id).all()
+    else:
+        query_obats = db.query(
+                Obat
+            ).filter(
+                Obat.nama.contains(query)
+            ).all()
+        query_obats_id = [ob.id for ob in query_obats]
+        barangs = db.query(
+                Barang
+            ).filter(
+                or_(
+                    Barang.batch.contains(query),
+                    Barang.obat_id.in_(query_obats_id)
+                )
+            ).order_by(
+                Barang.id
+            ).all()
+
+    print(barangs)
     return CustomTemplateResponse("produk/index.html", {"request": request, "user": user, 'barangs': barangs, 'obats': obats, 'message': message})
 
 
